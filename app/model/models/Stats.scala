262cdc77d2a4d4d644ed3d8aacf4a83f933627dc
Richard Portsmith <gifted.primate@protonmail.com>
2018-05-04 12:01:28 -0400

    removes gameComplete field from stats

--- app/model/models/Stats.scala
+++ app/model/models/Stats.scala
@@ -8,14 +8,12 @@ object Stats {
   val statsReads: Reads[Stats] = (
     (JsPath \ "level").read[String] and
     (JsPath \ "step").read[String] and
-    (JsPath \ "gameComplete").readNullable[Boolean] and
     (JsPath \ "levels").read[Map[String, Map[String, StepToken]]]
   )(Stats.apply _)
 
   val statsWrites: Writes[Stats] = (
     (JsPath \ "level").write[String] and
     (JsPath \ "step").write[String] and
-    (JsPath \ "gameComplete").writeNullable[Boolean] and
     (JsPath \ "levels").write[Map[String, Map[String, StepToken]]]
   )(unlift(Stats.unapply))
 
@@ -26,6 +24,5 @@ object Stats {
 case class Stats(
     level: String,
     step: String,
-    gameComplete: Option[Boolean] = Some(false),
     levels: Map[String, Map[String, StepToken]]
 )
