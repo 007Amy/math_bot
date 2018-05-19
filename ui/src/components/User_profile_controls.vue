a5d00d356814bc9ddbfcca1061e33ae596f6a24a
Richard Portsmith <gifted.primate@protonmail.com>
2018-05-19 16:27:08 -0400

    fixes about route

--- ui/src/components/User_profile_controls.vue
+++ ui/src/components/User_profile_controls.vue
@@ -6,7 +6,7 @@
     <div class="user-profile-controls-text-container">
       <div class="mathbot-header">MATH<span class="line">_</span>BOT</div>
       <div class="user-profile-controls-text-footer">
-        <div class="text about" @click="$router.push({path: '/marketing'})">About</div>
+        <div class="text about" @click="gotoMarketing">About</div>
         <div class="text sign-out" v-if="auth !== null" @click="auth.logout()">Sign Out</div>
       </div>
     </div>
@@ -16,6 +16,11 @@
 <script>
 export default {
   name: 'user_profile_controls',
+  methods: {
+    gotoMarketing () {
+      this.$router.push({path: '/about'})
+    }
+  },
   props: ['auth', 'permanentImages']
 }
 </script>
